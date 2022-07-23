dfb32b6242ffebed0032870d9436572766ca8f69
salozzo86 <antonio.malafronte86@gmail.com>
2022-07-23 19:10:17 +0200

    PlusIcon
    Styling

--- src/components/AddItemButton.js
+++ src/components/AddItemButton.js
@@ -1,8 +1,9 @@
 import Button from 'react-bootstrap/Button';
+import { PlusIcon } from '../plus-icon';
 
 const AddItemButton = ({ handleShow }) => {
     return (
-        <Button variant="success" className="mb-4" onClick={handleShow} >Add item</Button>
+        <Button variant="success" className="mb-4" onClick={handleShow} ><PlusIcon onClick={handleShow}></PlusIcon></Button>
 
 
     )
